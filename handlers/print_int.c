f67e9cc6a9948b1de4f0d6194eff13711ca2c60f
root <root@ip-10-42-28-200.ec2.internal>
2025-10-01 08:10:20 +0000

    Fix Task 1: Correct %d and %i logic

--- handlers/print_int.c
+++ handlers/print_int.c
@@ -1,37 +1,50 @@
 #include "../main.h"
 
 /**
- * put_snum - print a signed long integer recursively
- * @n: value
+ * print_number - recursively prints digits of an integer
+ * @n: integer to print
  * Return: number of characters printed
  */
-static int put_snum(long n)
+static int print_number(int n)
 {
 	int count = 0;
 
-	if (n < 0)
-	{
-		count += _putchar('-');
-		n = -n;
-	}
-
 	if (n / 10)
-		count += put_snum(n / 10);
+		count += print_number(n / 10);
 
 	count += _putchar('0' + (n % 10));
-
 	return (count);
 }
 
 /**
- * print_int - handler for %d and %i (signed int)
- * @ap: variadic argument list
+ * print_int - prints an integer (%d or %i)
+ * @ap: va_list with the int to print
  * Return: number of characters printed
  */
 int print_int(va_list ap)
 {
-	long n = (long)va_arg(ap, int);
+	int n = va_arg(ap, int);
+	int count = 0;
 
-	return (put_snum(n));
+	if (n == 0)
+		return (_putchar('0'));
+
+	if (n < 0)
+	{
+		count += _putchar('-');
+		/* handle INT_MIN safely */
+		if (n == -2147483648)
+		{
+			count += _putchar('2');
+			n = 147483648;
+		}
+		else
+		{
+			n = -n;
+		}
+	}
+
+	count += print_number(n);
+	return (count);
 }
 
